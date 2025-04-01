c4e4581744ffef0914615ab9a83c01e560c4d276
AbdulMalek <M_2008_HA@YAHOO.COM>
2025-04-01 15:01:22 -0400

    feat:
    - added right arm into the angle calculation logic.
    - Added a separate repCounter for the right arm.

--- pose_detection.py
+++ pose_detection.py
@@ -7,8 +7,11 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils  # For drawing landmarks
 
+
 cap = cv2.VideoCapture(0)
 counter = RepCounter()
+counter2 = RepCounter()
+
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -19,7 +22,6 @@ while cap.isOpened():
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(frame_rgb)
 
-    # Draw landmarks on the frame
     # Inside the loop:
     if results.pose_landmarks:
         landmarks = results.pose_landmarks.landmark
@@ -27,9 +29,17 @@ while cap.isOpened():
         elbow = landmarks[mp_pose.PoseLandmark.LEFT_ELBOW]
         wrist = landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
 
+        landmarks2 = results.pose_landmarks.landmark
+        shoulder2 = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        elbow2 = landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW]
+        wrist2 = landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
+
         angle = calculate_angle(shoulder, elbow, wrist)
+        angle2 = calculate_angle(shoulder2, elbow2, wrist2)
         reps = counter.update(angle)
-        cv2.putText(frame, f"Reps: {reps}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        reps2 = counter2.update(angle2)
+        cv2.putText(frame, f"Left Reps: {reps}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(frame, f"Right Reps: {reps2}", (380, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Pose Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
