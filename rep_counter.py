d2bae418b2fbd24958942ef9082e8c0e5d1979ab
AbdulMalek <M_2008_HA@YAHOO.COM>
2025-04-21 15:13:31 -0400

    - Added a time Threshold for better accuracy.

--- rep_counter.py
+++ rep_counter.py
@@ -1,17 +1,24 @@
+import time
+
+
 class RepCounter:
-    def __init__(self, contraction_threshold=30, extension_threshold=160):
+    def __init__(self, contraction_threshold=30, extension_threshold=160, min_rep_time=0.5):
         self.contraction_threshold = contraction_threshold
         self.extension_threshold = extension_threshold
         self.reps = 0
         self.state = "extended"
+        self.last_rep_time= time.time()
+        self.min_rep_time = min_rep_time
 
     def update(self, angle):
-        if self.state == "extended":
-            if angle < self.contraction_threshold:
+        current_time = time.time()
+        if (current_time - self.last_rep_time) < self.min_rep_time:
+            return self.reps
+        if self.state == "extended" and angle < self.contraction_threshold:
                 self.state = "contracted"
-        elif self.state == "contracted":
-            if angle > self.extension_threshold:
-                self.state = "extended"
-                self.reps += 1
-                print("count", self.reps)
+        elif self.state == "contracted" and angle > self.extension_threshold:
+            self.state = "extended"
+            self.reps += 1
+            self.last_rep_time = current_time
+            print(f"Angle: {angle:.1f}° | State: {self.state} | Reps: {self.reps}")
         return self.reps
